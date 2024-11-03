b04de1e894f6a2c8bc63393b078eefcf7e1f74f5
rogancodes <rogangs2@gmail.com>
2024-11-03 08:35:14 +0530

    Initial setup

---
+++ index.md
@@ -0,0 +1,9 @@
+---
+layout: default
+title: "Happy Jekylling!"
+---
+
+## You're ready to go!
+<p class="text-3xl font-bold underline">
+Start developing your Jekyll website.
+</p>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./_site/**/*.html", // Jekyll output files
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./*.md",
+    "./*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
